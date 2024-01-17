3dd6faef1b9ee93951b9933c320a7db32c05606c
Jaadoooo <jaadooogaming@gmail.com>
2024-01-18 01:02:02 +0530

    35

--- Lec 9 (Arrays)/TripletSum.cpp
+++ Lec 9 (Arrays)/TripletSum.cpp
@@ -3,3 +3,49 @@
 
 // An array is said to have a triplet {ARR[i], ARR[j], ARR[k]} with sum = 'K' if there exists three indices i, j and k 
 // such that i!=j, j!=k and i!=j and ARR[i] + ARR[j] + ARR[k] = 'K'.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    int s = 12;
+    vector<vector<int>> ans;
+
+    for(int i=0; i<5; i++)
+    {
+        for(int j=i+1; j<5; j++)
+        {
+            for(int k=0; k<5; k++)
+            {
+                if(arr[i] + arr[j] + arr[k]== s && i!=j && j!=k && i!=j)
+                {
+                    vector<int> temp;
+                    temp.push_back(arr[i]);
+                    temp.push_back(arr[j]);
+                    temp.push_back(arr[k]);
+                    sort(temp.begin(), temp.end());
+                    ans.push_back(temp);
+                }
+            } 
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+    
+    auto uniqueEnd = unique(ans.begin(), ans.end());
+    ans.erase(uniqueEnd, ans.end());
+
+
+    for(const auto& row: ans) 
+    {
+        for(const auto& element: row) 
+        {
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+}
